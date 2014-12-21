85e33dc4c868007c00e1e04d237f810ffdb3951d
unknown <etyle902@gmail.com>
2014-12-21 22:05:05 +0100

    Now using filepath instead of os.

--- minserv.go
+++ minserv.go
@@ -1,24 +1,22 @@
 package main
 
 import (
-        "flag"
-        "fmt"
-        "net/http"
-        "os"
-        "strconv"
+	"flag"
+	"fmt"
+	"net/http"
+	"path/filepath"
 )
 
 var path = flag.String("path", ".", "directory to serve")
 var port = flag.Int("port", 8080, "port to use")
 
 func main() {
-        flag.Parse()
-        var wd string
-        if *path == "." {
-                wd, _ = os.Getwd()
-        } else {
-                wd = *path
-        }
-        fmt.Printf("Serving %v on port %v...\n", wd, *port)
-        http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(wd)))
+	flag.Parse()
+	wd, err := filepath.Abs(*path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	portStr := fmt.Sprintf(":%v", *port)
+	fmt.Printf("Serving %v on port %v...\n", wd, portStr)
+	fmt.Println(http.ListenAndServe(portStr, http.FileServer(http.Dir(wd))))
 }
