03edb1fb357cecfeee00459a611bef26febbf532
unknown <etyle902@gmail.com>
2014-12-21 22:22:16 +0100

    improved error handling, check that directory exists

--- minserv.go
+++ minserv.go
@@ -4,19 +4,27 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
-var path = flag.String("path", ".", "directory to serve")
-var port = flag.Int("port", 8080, "port to use")
+var path = flag.String("path", ".", "Directory to serve.")
+var port = flag.Int("port", 8080, "Port to use.")
 
 func main() {
 	flag.Parse()
 	wd, err := filepath.Abs(*path)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	portStr := fmt.Sprintf(":%v", *port)
-	fmt.Printf("Serving %v on port %v...\n", wd, portStr)
-	fmt.Println(http.ListenAndServe(portStr, http.FileServer(http.Dir(wd))))
+	if info, err := os.Stat(wd); !info.IsDir() || os.IsNotExist(err) {
+		fmt.Println(*path, "does not exist or is not a directory.")
+		return
+	}
+
+	portStr := fmt.Sprint(*port)
+
+	fmt.Printf("MinServ: Now serving %v on port %v...\n", wd, portStr)
+	fmt.Println(http.ListenAndServe(":"+portStr, http.FileServer(http.Dir(wd))))
 }
